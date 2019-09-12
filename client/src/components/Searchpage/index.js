4bada4ca718f4de01f1b3069374b053abf6fb97e
Alex Thompson <alexthomps11@gmail.com>
2019-09-11 20:48:12 -0400

    showing search results

--- client/src/components/Searchpage/index.js
+++ client/src/components/Searchpage/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import axios from "axios";
 require("dotenv").config();
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-// const API_KEY = "2aee743ec443e651b7c676b14cc46d1c";
 class Searchpage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "300",
+      input: "",
       movies: []
     };
   }
@@ -18,15 +17,39 @@ class Searchpage extends React.Component {
     let resp = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.input}&page=1&include_adult=false`
     );
-    this.setState({ movies: [...resp.data.results] });
+    await this.setState({ movies: [...resp.data.results] });
   };
 
   componentDidMount = async () => {
     await this.fetchData();
   };
 
+  handleInput = e => {
+    e.preventDefault();
+    let { value } = e.target;
+    this.setState({ input: value });
+  };
+
+  handleSearch = async e => {
+    e.preventDefault();
+    this.fetchData();
+  };
+
+  renderMovies = e => {
+    return this.state.movies.map(movie => {
+      return <div>{movie.title}</div>;
+    });
+  };
+
   render() {
-    return <div>Movies</div>;
+    return (
+      <div>
+        <input onChange={this.handleInput}></input>
+        <button onClick={this.handleSearch}>Search</button>
+        Movies
+        {this.state.movies != [] ? this.renderMovies() : null}
+      </div>
+    );
   }
 }
 
